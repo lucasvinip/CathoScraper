5b90472ef3b28d1617b73474543d50206bbbc334
Lucas Vinicius <l.vinicius1577@gmail.com>
2025-08-02 01:12:14 -0300

    loggin in site and scroll window

--- Scraper/dist/index.js
+++ Scraper/dist/index.js
@@ -1,7 +1,37 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chicken_1 = require("./chicken");
-console.log('h');
-const chicken = new chicken_1.Chicken();
-chicken.cluck();
+const puppeteer = require('puppeteer');
+const url = 'http://localhost:5173';
+// const main = async () =>{
+// }
+async function main() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+    });
+    //args: ['--start-maximized']
+    const page = await browser.newPage();
+    await page.goto(url);
+    console.log('antes do evaluate');
+    await page.evaluate(async () => {
+        const distance = 100;
+        let scrolledAmount = 0;
+        return new Promise(resolve => {
+            const timer = setInterval(() => {
+                window.scrollBy(0, distance);
+                scrolledAmount += distance;
+                if (scrolledAmount >= document.body.scrollHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+    console.log('depois do evaluate');
+    // const element = await page.waitForSelector('div > .sendApplication > button');
+    // if (element) {
+    //     const text = await element.evaluate((el: HTMLButtonElement) => el.textContent, element);
+    //     console.log('encontrado', text);
+    // }
+}
+main();
 //# sourceMappingURL=index.js.map
--- Scraper/src/index.ts
+++ Scraper/src/index.ts
@@ -1,7 +1,48 @@
-import { Chicken } from "./chicken";
+const puppeteer = require('puppeteer');
 
-console.log('');
+const url = 'http://localhost:5173';
 
-const chicken = new Chicken();
-chicken.cluck();
+// const main = async () =>{
 
+// }
+
+async function main() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+    });
+     //args: ['--start-maximized']
+
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    console.log('antes do evaluate');
+
+    await page.evaluate(async () => {
+        const distance = 100;
+        let scrolledAmount = 0;
+
+        return new Promise<void>(resolve => {
+            const timer = setInterval(() => {
+                window.scrollBy(0, distance);
+                scrolledAmount += distance;
+
+                if (scrolledAmount >= document.body.scrollHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        })
+    });
+    console.log('depois do evaluate');
+    
+    // const element = await page.waitForSelector('div > .sendApplication > button');
+    // if (element) {
+    //     const text = await element.evaluate((el: HTMLButtonElement) => el.textContent, element);
+    //     console.log('encontrado', text);
+
+    // }
+
+}
+
+main();
